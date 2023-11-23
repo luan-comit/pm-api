19a557f8706e1f16903b229ecb3f95569c6558cb
louisle01 <louis_le_01@yahoo.com>
2023-11-23 09:34:40 -0600

    port 80

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT
 
-app.set('port', process.env.PORT || 8080)
+app.set('port', port || 8080)
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
