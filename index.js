27680b6d4dc7d65091fd457a4b30337fb2d85432
Louis Le <fptnet1280@yahoo.com>
2023-11-23 10:39:10 -0700

    PORT default

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
-
-app.set('port', port || 8080)
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
